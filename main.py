e808ea29691123569de33243ba345669fa1a3ee9
Ali <gueyea1@tcnj.edu>
2021-08-20 02:40:51 -0400

    Added business logic for VR PUT and utils.py for helper functions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import datetime
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse, fields, marshal_with, inputs, abort
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime as dt, timedelta
+import utils
+import pytz
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Vacation request database model
 class Vacation_Request_Model(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.Integer, nullable=False)
@@ -26,6 +30,7 @@ class Vacation_Request_Model(db.Model):
 db.create_all()
 
 # FIXME: '' xor ""
+# Input verification
 vacation_request_put_args = reqparse.RequestParser()
 vacation_request_put_args.add_argument("id", type=int, help="ID required")
 vacation_request_put_args.add_argument("author", type=int, help="Worker ID required", required=True)
@@ -69,19 +74,44 @@ class Vacation_Request(Resource):
     @marshal_with(resource_fields)
     def put(self, vr_id):
         args = vacation_request_put_args.parse_args()
+        start = args['vacation_start_date'].timestamp()
+        end = args['vacation_end_date'].timestamp()
+        now = dt.today().timestamp()
+        vacation_days_used = 0
 
-        # FIXME: what if already exist or id is taken?
+        # Check if id is unique
+        result = Vacation_Request_Model.query.get(vr_id)
+        if result:
+            abort(400, message="Vacation Request ID is not unique")
 
-        # FIXME: check if date makes sense
-        # - ahead of present time
-        # - start < end
-        # - less than 30 total request
-        # - vacations cannot overlap
+        # Check if vacation begins in the past
+        if now > start:
+            abort(400, message='Vacation must take place in present')
+        
+        # Check if vacation starts after end date
+        if start > end:
+            abort(400, message='Vacation cannot start after end')
 
+        # Gets all vacation request from author
+        result = Vacation_Request_Model.query.filter_by(author = args['author']).all()
 
-        # FIXME: update datetime to current for created at
+        # Counts workdays included in vacation 
+        if result:
+            for request in result:
+                vacation_days_used += utils.work_days_used(request.vacation_start_date, request.vacation_end_date)
+            
+            # Checks if vacation limit has been reached by previous requests
+            if vacation_days_used >= 30:
+                abort(400, message="Employee has reached vacation day limit")
+
+        # Checks if vacation limit has been reached by current requests
+        vacation_days_used += utils.work_days_used(args['vacation_start_date'], args['vacation_end_date'])
+        if vacation_days_used >=30:
+            abort(400, message=f'Your vacation is too long. Only {30 - vacation_days_used} left')
+
+        # Creates new instance of Vacation Request Model
         vacation_request = Vacation_Request_Model(id=vr_id, author=args['author'], status=args['status'], 
-                                                  resolved_by=args['resolved_by'], request_created_at=args['request_created_at'], 
+                                                  resolved_by=args['resolved_by'], request_created_at=dt.now(tz=pytz.utc), 
                                                   vacation_start_date=args['vacation_start_date'], vacation_end_date=args['vacation_end_date'])
 
         db.session.add(vacation_request)
@@ -99,6 +129,10 @@ class Vacation_Request(Resource):
 
         if args['status']:
             result.status = args['status']
+        if args['vacation_start_date']:
+            result.vacation_start_date = args['vacation_start_date']
+        if args['vacation_end_date']:
+            result.vacation_end_date = args['vacation_end_date']
 
         db.session.commit()
 
---
+++ utils.py
@@ -0,0 +1,14 @@
+from datetime import datetime as dt, timedelta
+
+FEDERAL_HOLIDAYS = [dt(2021,9,6), dt(2021,10,11), dt(2021,11,11), dt(2021, 11, 25), dt(2021,12,24), dt(2022,1,1), dt(2022,1,17), dt(2022,5,30), dt(2022,7,4), dt(2022,9,5)]
+
+# Calculates number of work days used
+def work_days_used(start, end):
+    day_incrementor = start
+    days_used = 0
+    for _ in range((end - start).days):
+        if day_incrementor.weekday() < 5 and day_incrementor not in FEDERAL_HOLIDAYS:
+            days_used += 1
+
+            day_incrementor += timedelta(days=1)
+    return days_used
